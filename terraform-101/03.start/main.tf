b2eb043b6f5b9d23a1074cf4c774a6e5078002a9
Been Son <beengineer0420@gmail.com>
2025-06-23 21:21:37 +0900

    Delete terraform-101/03.start directory

--- terraform-101/03.start/main.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "abc" {
-  content  = "abc!"
-  filename = "${path.module}/abc.txt"
-}
-
-resource "local_file" "def" {
-  content  = "def!"
-  filename = "${path.module}/def.txt"
-}
